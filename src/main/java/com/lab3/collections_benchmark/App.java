a106d0e5b42018966e112a30ef18c8d3c72a46ea
Митюшин Максим <mityushin-2005@mail.ru>
2026-04-29 21:45:51 +0300

    Initial commit

---
+++ src/main/java/com/lab3/collections_benchmark/App.java
@@ -0,0 +1,7 @@
+package com.lab3.collections_benchmark;
+
+public class App {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+}
---
+++ src/test/java/com/lab3/collections_benchmark/AppTest.java
@@ -0,0 +1,13 @@
+package com.lab3.collections_benchmark;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest {
+    
+  @Test
+  public void shouldAnswerWithTrue() {
+    assertTrue(true);
+  }
+}
