3792ad20284100b059bb075bf9c6ae8577913ed5
Митюшин Максим <mityushin-2005@mail.ru>
2026-04-29 23:23:42 +0300

    Добавлены замеры тех же методов для LinkedList

--- src/test/java/com/lab3/collections_benchmark/CollectionsBenchmark.java
+++ src/test/java/com/lab3/collections_benchmark/CollectionsBenchmark.java
@@ -1,6 +1,7 @@
 package com.lab3.collections_benchmark;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.Random;
 
 public class CollectionsBenchmark {
@@ -8,7 +9,7 @@ public class CollectionsBenchmark {
 	public static final int OPERATIONS_COUNT = 20000;
     private static final Random RANDOM = new Random();
 
-    public static long measureAdd() {
+    public static long measureAddArrayList() {
         ArrayList<Integer> list = new ArrayList<>();
         long start = System.nanoTime();
         for (int i = 0; i < OPERATIONS_COUNT; i++) {
@@ -17,7 +18,7 @@ public class CollectionsBenchmark {
         return System.nanoTime() - start;
     }
 
-    public static long measureGet() {
+    public static long measureGetArrayList() {
         ArrayList<Integer> list = new ArrayList<>();
         for (int i = 0; i < OPERATIONS_COUNT; i++) {
             list.add(i);
@@ -30,11 +31,43 @@ public class CollectionsBenchmark {
         return System.nanoTime() - start;
     }
 
-    public static long measureRemove() {
+    public static long measureRemoveArrayList() {
         ArrayList<Integer> list = new ArrayList<>();
         for (int i = 0; i < OPERATIONS_COUNT; i++) {
             list.add(i);
         }
+        long start = System.nanoTime();
+        for (int i = 0; i < OPERATIONS_COUNT; i++) {
+            if (list.isEmpty()) break;
+            int index = RANDOM.nextInt(list.size());
+            list.remove(index);
+        }
+        return System.nanoTime() - start;
+    }
+    
+    public static long measureAddLinkedList() {
+        LinkedList<Integer> list = new LinkedList<>();
+        long start = System.nanoTime();
+        for (int i = 0; i < OPERATIONS_COUNT; i++) {
+            list.add(i);
+        }
+        return System.nanoTime() - start;
+    }
+
+    public static long measureGetLinkedList() {
+        LinkedList<Integer> list = new LinkedList<>();
+        for (int i = 0; i < OPERATIONS_COUNT; i++) list.add(i);
+        long start = System.nanoTime();
+        for (int i = 0; i < OPERATIONS_COUNT; i++) {
+            int index = RANDOM.nextInt(list.size());
+            list.get(index);
+        }
+        return System.nanoTime() - start;
+    }
+
+    public static long measureRemoveLinkedList() {
+        LinkedList<Integer> list = new LinkedList<>();
+        for (int i = 0; i < OPERATIONS_COUNT; i++) list.add(i);
         long start = System.nanoTime();
         for (int i = 0; i < OPERATIONS_COUNT; i++) {
             if (list.isEmpty()) break;
--- src/test/java/com/lab3/collections_benchmark/Main.java
+++ src/test/java/com/lab3/collections_benchmark/Main.java
@@ -2,18 +2,28 @@ package com.lab3.collections_benchmark;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Сравнение производительности ArrayList");
+		System.out.println("Сравнение производительности ArrayList и LinkedList");
         System.out.println("Количество операций: " + CollectionsBenchmark.OPERATIONS_COUNT);
         System.out.println("--------------------------------------------------");
-        System.out.printf("%-10s | %-15s | %-15s%n", "Операция", "Время (нс)", "Время (мс)");
+        System.out.printf("%-10s | %-12s | %-15s | %-15s%n", "Операция", "Коллекция", "Время (нс)", "Время (мс)");
         System.out.println("--------------------------------------------------");
 
-        long addTime = CollectionsBenchmark.measureAdd();
-        long getTime = CollectionsBenchmark.measureGet();
-        long removeTime = CollectionsBenchmark.measureRemove();
 
-        System.out.printf("%-10s | %-15d | %-15d%n", "add", addTime, addTime / 1000000);
-        System.out.printf("%-10s | %-15d | %-15d%n", "get", getTime, getTime / 1000000);
-        System.out.printf("%-10s | %-15d | %-15d%n", "remove", removeTime, removeTime / 1000000);
+        printResult("add", "ArrayList", CollectionsBenchmark.measureAddArrayList());
+        printResult("get", "ArrayList", CollectionsBenchmark.measureGetArrayList());
+        printResult("remove", "ArrayList", CollectionsBenchmark.measureRemoveArrayList());
+        
+        System.out.println("--------------------------------------------------");
+        
+        printResult("add", "LinkedList", CollectionsBenchmark.measureAddLinkedList());
+        printResult("get", "LinkedList", CollectionsBenchmark.measureGetLinkedList());
+        printResult("remove", "LinkedList", CollectionsBenchmark.measureRemoveLinkedList());
+        
+        System.out.println("--------------------------------------------------");
+    }
+	
+	private static void printResult(String op, String col, long timeNs) {
+        long timeMs = timeNs / 1_000_000;
+        System.out.printf("%-10s | %-12s | %-15d | %-15d%n", op, col, timeNs, timeMs);
     }
 }
