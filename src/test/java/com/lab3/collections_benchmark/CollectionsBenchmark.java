3f9f7f9cc441628eb21ae85baa034bdf8eb4ac32
Митюшин Максим <mityushin-2005@mail.ru>
2026-04-29 22:58:19 +0300

    Замер скорости для ArrayList add(в конец), get(random), remove(random)

--- src/test/java/com/lab3/collections_benchmark/CollectionsBenchmark.java
+++ src/test/java/com/lab3/collections_benchmark/CollectionsBenchmark.java
@@ -1,5 +1,46 @@
 package com.lab3.collections_benchmark;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class CollectionsBenchmark {
 
+	public static final int OPERATIONS_COUNT = 20000;
+    private static final Random RANDOM = new Random();
+
+    public static long measureAdd() {
+        ArrayList<Integer> list = new ArrayList<>();
+        long start = System.nanoTime();
+        for (int i = 0; i < OPERATIONS_COUNT; i++) {
+            list.add(i);
+        }
+        return System.nanoTime() - start;
+    }
+
+    public static long measureGet() {
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 0; i < OPERATIONS_COUNT; i++) {
+            list.add(i);
+        }
+        long start = System.nanoTime();
+        for (int i = 0; i < OPERATIONS_COUNT; i++) {
+            int index = RANDOM.nextInt(list.size());
+            list.get(index);
+        }
+        return System.nanoTime() - start;
+    }
+
+    public static long measureRemove() {
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 0; i < OPERATIONS_COUNT; i++) {
+            list.add(i);
+        }
+        long start = System.nanoTime();
+        for (int i = 0; i < OPERATIONS_COUNT; i++) {
+            if (list.isEmpty()) break;
+            int index = RANDOM.nextInt(list.size());
+            list.remove(index);
+        }
+        return System.nanoTime() - start;
+    }
 }
--- src/test/java/com/lab3/collections_benchmark/Main.java
+++ src/test/java/com/lab3/collections_benchmark/Main.java
@@ -1,5 +1,19 @@
 package com.lab3.collections_benchmark;
 
 public class Main {
+	public static void main(String[] args) {
+		System.out.println("Сравнение производительности ArrayList");
+        System.out.println("Количество операций: " + CollectionsBenchmark.OPERATIONS_COUNT);
+        System.out.println("--------------------------------------------------");
+        System.out.printf("%-10s | %-15s | %-15s%n", "Операция", "Время (нс)", "Время (мс)");
+        System.out.println("--------------------------------------------------");
 
+        long addTime = CollectionsBenchmark.measureAdd();
+        long getTime = CollectionsBenchmark.measureGet();
+        long removeTime = CollectionsBenchmark.measureRemove();
+
+        System.out.printf("%-10s | %-15d | %-15d%n", "add", addTime, addTime / 1000000);
+        System.out.printf("%-10s | %-15d | %-15d%n", "get", getTime, getTime / 1000000);
+        System.out.printf("%-10s | %-15d | %-15d%n", "remove", removeTime, removeTime / 1000000);
+    }
 }
